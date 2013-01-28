50049bce50516922e2ee3327413db33432699ee6
Alex Johnson <ajohnson@nearinfinity.com>
2013-01-28 10:08:09 -0500

    Flushed out test for pcapConverter.

--- test/pcapConverterTest.js
+++ test/pcapConverterTest.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var assert = require('assert');
 var tshark = require('../');
 var path = require('path');
 var fs = require('fs');
@@ -8,24 +8,50 @@ var testDataPath = path.resolve(__dirname, '../testData');
 
 module.exports = {
   "convert pcap file to tshark": function(test) {
+    var errOccurred = false;
+    var madeFile = false;
     var pcapConverter = new tshark.PcapConverter();
     pcapConverter.convertFile(path.resolve(testDataPath, 'http.pcap'));
+    pcapConverter.on('error', function(err){
+      errOccurred = true;
+      return test.done(err);
+    });
     pcapConverter.on('data', function(data) {
-      test.fail('test that data is correct');
+      //test.fail('test that data is correct');
+      //Test that the file is created correctly
+      fs.open(path.resolve(testDataPath, 'http.tshark'), 'r', function(err, fd){
+        if (!err) madeFile = true;
+      });
     });
     pcapConverter.on('done', function() {
-      test.done();
+      if (!madeFile)
+        return test.fail('A file was not made');
+      if (!errOccurred)
+        return test.done();
+      return 0;
     });
   },
 
   "convert pcap stream tshark": function(test) {
+    var errOccurred = false;
+    var madeFile = false;
     var pcapConverter = new tshark.PcapConverter();
     pcapConverter.convertStream(fs.createReadStream(path.resolve(testDataPath, 'http.pcap')));
+    pcapConverter.on('error', function(err){
+      errOccurred = true;
+      return test.done(err);
+    });
     pcapConverter.on('data', function(data) {
-      test.fail('test that data is correct');
+      fs.open(path.resolve(testDataPath, 'http.tshark'), 'r', function(err, fd){
+        if (!err) madeFile = true;
+      });
     });
     pcapConverter.on('done', function() {
-      test.done();
+      if (!madeFile)
+        return test.fail('A file was not made');
+      if (!errOccurred)
+        return test.done();
+      return 0;
     });
   }
 };
