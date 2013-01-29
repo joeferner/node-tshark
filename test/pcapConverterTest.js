c56e757499fe3f83cdda518fe5d1262fb54dfe5f
Alex Johnson <ajohnson@nearinfinity.com>
2013-01-29 09:00:10 -0500

    Latest changes. Didn't get around to getting buffer information written in last night, but it's the order in the globalHeader function.

--- test/pcapConverterTest.js
+++ test/pcapConverterTest.js
@@ -15,50 +15,33 @@ module.exports = {
     pcapConverter.on('error', function(err){
       errOccurred = true;
       console.log("@@@@@@@@@ There was an error");
-      return test.done(err);
+      return test.fail(err);
     });
     pcapConverter.on('data', function(data) {
       //test.fail('test that data is correct');
       //Test that the file is created correctly
       fs.open(path.resolve(testDataPath, 'http.tshark'), 'r', function(err, fd){
-        if (!err) {
-          madeFile = true;
+        if (err) 
+          throw err;
+        else {
+          return test.done();
         }
-        if (err) throw err;
       });
-    });
-    pcapConverter.on('done', function() {
-      if (!madeFile){
-        test.fail('A file was not made');
-        return test.done();
-      }
-      if (!errOccurred)
-        console.log("######### Finishing test");
-        return test.done();
-      return 0;
     });
   },
 
   "convert pcap stream tshark": function(test) {
     var errOccurred = false;
-    var madeFile = false;
     var pcapConverter = new tshark.PcapConverter();
     pcapConverter.convertStream(fs.createReadStream(path.resolve(testDataPath, 'http.pcap')));
     pcapConverter.on('error', function(err){
       errOccurred = true;
-      return test.done(err);
+      return test.fail(err);
     });
     pcapConverter.on('data', function(data) {
       fs.open(path.resolve(testDataPath, 'http.tshark'), 'r', function(err, fd){
-        if (!err) madeFile = true;
+        if (!err) test.done();
       });
-    });
-    pcapConverter.on('done', function() {
-      if (!madeFile)
-        return test.fail('A file was not made');
-      if (!errOccurred)
-        return test.done();
-      return 0;
     });
   }
 };
